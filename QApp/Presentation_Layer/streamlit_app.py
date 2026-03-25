0eaea7081b602a8b4956e2f8a311b4021f7c8f7f
Hamid Bekamiri <hamidb201214@gmail.com>
2026-03-25 09:54:12 +0000

    Simple QApp

--- QApp/Presentation_Layer/streamlit_app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("QApp Demo")
-question = st.text_input("Ask a question")
-
-if question:
-    st.write(f"You asked: {question}")
-    st.write("This is a demo answer from the app.")
